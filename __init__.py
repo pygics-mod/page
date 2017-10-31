78be33fc0d7f0701ee34c83f5707fdd7b6e43ab2
HyechurnJang <hyjang@cisco.com>
2017-10-31 14:50:31 +0900

    update

--- __init__.py
+++ __init__.py
@@ -4,14 +4,6 @@ Created on 2017. 5. 30.
 @author: HyechurnJang
 '''
 
-# from tags import TAG
-# from tags import DIV, SPAN, ROW, COL, IFRAME, NAV, SCRIPT
-# from tags import HEAD, PARA, ANCH, LABEL, STRONG, SMALL
-# from tags import IMG, ICON
-# from tags import THEAD, TBODY, TH, TR, TD, TABLE
-# from tags import UL, LI
-# from tags import FORM, INPUT, BUTTON
-
 from pygics import ContentType, export
 from core import Page
 from tags import Tag
